d0ec7ada9e32bfae0730164792ef0a47b8ad3495
Jose Manuel <jose@261004@gmail.com>
2021-07-29 10:50:25 -0400

    Medatadatas and padding working

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
 const root = document.documentElement,
+    $form = document.getElementById('form'),
     $code = document.getElementById('code'),
     $widthInput = document.getElementById('widthInput'),
     $heightInput = document.getElementById('heightInput'),
     $paddingInput = document.getElementById('paddingInput'),
     $borderInput = document.getElementById('borderInput'),
     $boxSizing = document.getElementById('boxSizing'),
-    $boxSizingLabel = document.getElementById('boxSizingLabel'),
+    $marginInput = document.getElementById('marginInput'),
     $box = document.getElementById('box');
 
-$code.addEventListener('input', (e) => {
-    const $E = e.target;
+const operate = (number, sum) => (sum) ? number + 1 : number - 1;
+
+const setCSSVarible = (arr = [], value) => arr.forEach(E => root.style.setProperty(E, value));
+
+const changeBoxModel = (e) => {
+    const $E = e.target,
+        value = $E.value.trim();
+    if ($E.classList.contains('input') && value.match(/\d/gi).length > 0) {
+        if (e.key === "ArrowUp" || e.key === "ArrowDown")
+            $E.value = `${operate(parseInt(value.match(/\d/g).join('')), e.key === "ArrowUp")}${value.match(/[^\d]/g).join('')}`;
+    }
     switch ($E) {
         case $widthInput:
-            root.style.setProperty('--width', $widthInput.value.trim());
+            root.style.setProperty('--width', value);
             break;
         case $heightInput:
-            root.style.setProperty('--height', $heightInput.value.trim());
+            root.style.setProperty('--height', value);
             break;
         case $paddingInput:
-            root.style.setProperty('--padding', $paddingInput.value.trim());
+            root.style.setProperty('--padding', value);
             break;
         case $borderInput:
-            root.style.setProperty('--border-width', $borderInput.value.trim());
+            root.style.setProperty('--border-width', value);
             break;
         case $boxSizing:
             $box.classList.toggle('content-box');
             break;
     }
-});
+}
+$code.addEventListener('change', changeBoxModel);
+$code.addEventListener('keydown', changeBoxModel);
+$form.addEventListener('click', () => $form.toggleAttribute('hidden'));
