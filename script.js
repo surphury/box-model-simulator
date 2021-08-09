fc2d7520f54e4c6fb37b88f6c2b17eb5a47f40b8
Jose Manuel <jose@261004@gmail.com>
2021-08-09 16:09:36 -0400

    Version 1 Beta

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
+/* All HTMLElements have a "$" at the beggining */
+/* HTMLElements */
 const root = document.documentElement,
     $form = document.getElementById('form'),
     $code = document.getElementById('code'),
     $widthInput = document.getElementById('widthInput'),
     $heightInput = document.getElementById('heightInput'),
-    $paddingInput = document.getElementById('paddingInput'),
+    $paddingTopInput = document.getElementById('paddingTopInput'),
+    $paddingRightInput = document.getElementById('paddingRightInput'),
+    $paddingBottomInput = document.getElementById('paddingBottomInput'),
+    $paddingLeftInput = document.getElementById('paddingLeftInput'),
     $borderInput = document.getElementById('borderInput'),
     $boxSizing = document.getElementById('boxSizing'),
-    $marginInput = document.getElementById('marginInput'),
+    $marginInputTop = document.getElementById('marginInputTop'),
+    $marginInputRight = document.getElementById('marginInputRight'),
+    $marginInputBottom = document.getElementById('marginInputBottom'),
+    $marginInputLeft = document.getElementById('marginInputLeft'),
     $box = document.getElementById('box');
 
 const operate = (number, sum) => (sum) ? number + 1 : number - 1;
 
 const setCSSVarible = (arr = [], value) => arr.forEach(E => root.style.setProperty(E, value));
 
-const changeBoxModel = (e) => {
+const boxModelListener = (e) => {
     const $E = e.target,
         value = $E.value.trim();
-    if ($E.classList.contains('input') && value.match(/\d/gi).length > 0) {
-        if (e.key === "ArrowUp" || e.key === "ArrowDown")
-            $E.value = `${operate(parseInt(value.match(/\d/g).join('')), e.key === "ArrowUp")}${value.match(/[^\d]/g).join('')}`;
+
+    if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+        console.log(value.match(/\d/gi) !== 0);
+        if ($E.classList.contains('input') && value.match(/\d/gi).length !== 0) {
+            $E.value = `${operate(parseInt(value.match(/[-\d]/gi).join('')), e.key === "ArrowUp")}${value.match(/[^-\d]/gi).join('')}`;
+        }
+    } else if (value.length !== 0) {
+        changeBoxModel($E, value);
     }
+}
+
+const changeBoxModel = ($E, value) => {
     switch ($E) {
         case $widthInput:
             root.style.setProperty('--width', value);
@@ -27,21 +43,45 @@ const changeBoxModel = (e) => {
         case $heightInput:
             root.style.setProperty('--height', value);
             break;
-        case $paddingInput:
-            root.style.setProperty('--padding', value);
-            break;
         case $borderInput:
             root.style.setProperty('--border-width', value);
+            break;
+        case $paddingTopInput:
+            root.style.setProperty('--padding-top', value);
+            break;
+        case $paddingRightInput:
+            root.style.setProperty('--padding-right', value);
+            break;
+        case $paddingBottomInput:
+            root.style.setProperty('--padding-bottom', value);
+            break;
+        case $paddingLeftInput:
+            root.style.setProperty('--padding-left', value);
             break;
         case $boxSizing:
             $box.classList.toggle('content-box');
             break;
+        case $marginInputTop:
+            root.style.setProperty('--margin-top', value);
+            break;
+        case $marginInputRight:
+            root.style.setProperty('--margin-right', value);
+            break;
+        case $marginInputBottom:
+            root.style.setProperty('--margin-bottom', value);
+            break;
+        case $marginInputLeft:
+            root.style.setProperty('--margin-left', value);
+            break;
     }
 }
-$code.addEventListener('change', changeBoxModel);
-$code.addEventListener('keydown', changeBoxModel);
 
+
+$code.addEventListener('keypress', boxModelListener);
+$code.addEventListener('change', boxModelListener);
+$code.addEventListener('keyup', boxModelListener);
+/* 
 $form.addEventListener('click', (e) => {
     if (e.target.classList.contains('fieldset'))
         $form.toggleAttribute('hidden')
-});
+}); */
